186b24cc1a1a545c0d554214870c9ba18ecf4897
Andre Duarte <andre.bh@hotmail.com>
2022-01-23 21:10:36 -0300

    first code

--- phone.js
+++ phone.js
@@ -1,5 +1,4 @@
 function telephoneCheck(str) {
-    return true;
+    return (str.length > 9 && str.length < 14) && /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/.test(str);
 }
-
 telephoneCheck("555-555-5555");
